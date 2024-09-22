d3e0e92e8bcedac1dc1d133eca1993db9280f1fb
arunas006 <arun.a.s006@gmail.com>
2024-09-22 14:48:09 +0530

    additional features
    Included additional features which gets feedback from user

--- streamlit_code.py
+++ streamlit_code.py
@@ -22,6 +22,9 @@ col1, col2, col3 = st.columns(3)
 with col1:
     fuel_type=st.selectbox("Select The Fuel Type",["Diesel","Petrol","CNG","LPG","Electric"])
 
+with col2:
+    yr=st.selectbox("Select The Fuel Type",data["year"].unique())
+
 with col3:
     engine=st.slider("Required Engine Power",min_value=500,max_value=5000,step=100)
 
@@ -31,8 +34,22 @@ col4,col5,col6=st.columns(3)
 with col4:
     transmission_type=st.selectbox("Select The Required Transmission Type",["Manual","Automatic"])
 
+with col5:
+    seller_type=st.selectbox("Select The Seller Type",['Dealer','Individual','Trustmark Dealer'])
+
 with col6:
     seat=st.selectbox("Select The Required Passenger Capacity",[4,5,7,9,11])
+
+col7, col8, col9 = st.columns(3)
+
+with col7:
+    km_driven=st.slider("Expected KM RUN",min_value=500,max_value=4000000,step=10000)
+
+with col8:
+    mileage=st.slider("Required Mileage",min_value=10,max_value=150,step=3)
+
+with col9:
+    power=st.slider("Required Engine Power",min_value=5,max_value=800,step=20)
 
 encode_dict={
     "fuel_type":{'Diesel':1,'Petrol':2,'CNG':3,'LPG':4,'Electric':5},
@@ -40,19 +57,20 @@ encode_dict={
     "transmission_type":{'Manual':1,'Automatic':2}
 }
 
-def model_prediction(fuel_type,engine,transmission_type,seat):
+def model_prediction(fuel_type,engine,transmission_type,seat,yr,seller_type,km_driven,mileage,power):
     with open("car_pred_pickle",'rb') as file:
         reg_model=pickle.load(file)
 
     fuel=encode_dict["fuel_type"][fuel_type]
     transmission=encode_dict["transmission_type"][transmission_type]
+    seller=encode_dict["seller_type"][seller_type]
     
-    Input_feature=[[2018.0,1,4000,fuel,transmission,19.70,engine,86.30,seat]]
+    Input_feature=[[yr,seller,km_driven,fuel,transmission,mileage,engine,power,seat]]
 
     return np.round(reg_model.predict(Input_feature)[0],3)
 
 if st.button("PREDICT"):
-    output=model_prediction(fuel_type,engine,transmission_type,seat)
+    output=model_prediction(fuel_type,engine,transmission_type,seat,yr,seller_type,km_driven,mileage,power)
     st.write(f"The Price of Car which your looking for is {output} lakhs Rupees")
 else:
     st.write("Awaiting for Required features confirmation")
